e75058cd188cbd885dab4e5f6f7674ba562de409
matiasDev007 <mariomatias879@gmail.com>
2025-06-19 13:54:39 +0100

    Terminei a page contacto

--- kubona/src/app/sobre/page.tsx
+++ kubona/src/app/sobre/page.tsx
@@ -1,14 +1,13 @@
 import Header from "@/components/Layout/Header";
+import Main from './components/layout/Main'
+import NossaHistoria from "./components/layout/NossaHistoria";
 
 export default function Sobre() {
   return (
     <div>
       <Header />
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. At maiores qui
-        repudiandae quibusdam! Ullam maiores sit rerum saepe dicta accusamus
-        similique quia maxime. A sapiente, molestiae aut aliquam ea soluta.
-      </h1>
+      <Main/>
+      <NossaHistoria/>
     </div>
   );
 }
